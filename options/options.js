715b149687fdd96907f038442b09f3d841f223df
ishubham1995 <shubham.malik@crowdbotics.com>
2023-08-04 13:26:10 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "cdrt2-dev-107045",
-  url: "https://cdrt2-dev-107045.botics.co",
-  api: "https://cdrt2-dev-107045.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "cdrt2-dev-107045",
+  "url": "https://cdrt2-dev-107045.botics.co",
+  "api": "https://cdrt2-dev-107045.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
