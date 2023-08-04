fc6ac4c038636052410193c1ec5d68ecee08881e
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-04 06:20:33 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Sffgg,Sffgg,Sffgg
+from .serializers import SffggSerializer,SffggSerializer,SffggSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class SffggViewSet(viewsets.ModelViewSet):
+    serializer_class = SffggSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Sffgg.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Sffgg(models.Model):
+    'Generated Model'
+    ytryry = models.BigIntegerField()
+    cbcbc = models.BigIntegerField()
